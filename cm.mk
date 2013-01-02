7a97113f0e10cfc29384b5868004ea96fba3aa3d
Takazumi Matsumoto <tdr@cyanogenmod.org>
2013-01-02 18:53:04 +0000

    everest: change identifiers for compat
    Change-Id: I7c717c3fc3e24d9e8818fe30d932a6b71eeeaf4f

--- cm.mk
+++ cm.mk
@@ -13,9 +13,9 @@ $(call inherit-product, device/moto/everest/full_everest.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := everest
 PRODUCT_NAME := cm_everest
-PRODUCT_BRAND := MOTO
-PRODUCT_MODEL := MZ601
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Xoom
 PRODUCT_MANUFACTURER := Motorola
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=RTCOREEU BUILD_FINGERPRINT="MOTO/RTCOREEU/umts_everest:3.2/H.6.5-17-3/1321319666:user/ota-rel-keys,release-keys" PRIVATE_BUILD_DESC="umts_everest-user 3.2 H.6.5-17-3 1321319666 ota-rel-keys,release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=tervigon BUILD_FINGERPRINT="MOTO/RTCOREEU/umts_everest:3.2/H.6.5-17-3/1321319666:user/ota-rel-keys,release-keys" PRIVATE_BUILD_DESC="umts_everest-user 3.2 H.6.5-17-3 1321319666 ota-rel-keys,release-keys"
